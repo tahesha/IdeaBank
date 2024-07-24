0b2547e4054b730c11da8b186ee4a116a51d91ff
Tahesha <taheshao@gmail.com>
2024-07-24 12:05:08 -0400

    Added import and router functionality for App.

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import AddIdea from './pages/AddIdea';
+import Categories from './pages/Categories';
+import Random from './pages/Random';
+import './styles.css';
+
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/add" component={AddIdea} />
+                    <Route path="/categories" component={Categories} />
+                    <Route path="/random" component={Random} />
+                </Switch>
+                <Footer />
+            </div>
+        </Router>
+    );
+};
+
+export default App;
