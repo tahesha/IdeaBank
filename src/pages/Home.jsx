184a2db38ccacd8c4f18909d0c1b2b4b815bdbdb
Tahesha <taheshao@gmail.com>
2024-07-24 13:21:56 -0400

    Added functionality and connectivity to Home.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import IdeaForm from '../components/IdeaForm';
 import IdeaList from '../components/IdeaList';
 
 const Home = () => {
+    const [ideas, setIdeas] = useState([]);
+
+    const addIdea = (idea) => {
+        setIdeas([...ideas, idea]);
+    };
+
     return (
         <main>
             <h2>Welcome to Idea Bank</h2>
-            <IdeaList />
+            <IdeaForm addIdea={addIdea} />
+            <IdeaList ideas={ideas} />
         </main>
     );
 };
