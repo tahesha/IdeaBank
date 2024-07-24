1f512d870950371905aa50dde919734050776e31
Tahesha <taheshao@gmail.com>
2024-07-24 11:58:04 -0400

    Added Homepage info.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import IdeaList from '../components/IdeaList';
+
+const Home = () => {
+    return (
+        <main>
+            <h2>Welcome to Idea Bank</h2>
+            <IdeaList />
+        </main>
+    );
+};
+
+export default Home;
